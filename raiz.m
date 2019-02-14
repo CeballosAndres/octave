c72f84aefa01507fb4e246d0d9f0823fb5b5fb0e
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2019-02-13 23:30:03 -0600

    update

---
+++ raiz.m
@@ -0,0 +1,25 @@
+f = inline(input('Ingrese la función: ','s'));
+a = input('Ingrese valor para a: ');
+b = input('Ingrese valor para b: ');
+tolerancia = input('Ingrese la tolerancia permitida: ');
+seguir = true;
+x = 0;
+y = 0;
+
+if (f(a) * f(b) <0)
+    while(seguir)
+        x = (a + b)/2;
+        y = f(x);
+        if (((b-a)/2) < tolerancia)
+            seguir = false;
+        endif
+        if (f(a) * f(x) < 0)
+            b = x;
+        else
+            a = x;
+        endif
+    endwhile
+else
+    printf('La función no cruza el eje x en el intervalo dado.\n')
+endif
+printf('La raiz es x=%d  f(x)=%d \n',x,y) 
