3e92410a9893a93df602a21831bef95c447e7abe
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2019-02-28 21:13:19 -0600

    add newton raphson 2

--- raiz_newton_raphson_modificado.m
+++ raiz_newton_raphson_modificado.m
@@ -13,17 +13,13 @@ printf('Con el punto x0=%d\n',x0)
 fprintf('\t #  \t x0 \t\t x1 \t\t f(x1) \t\t Ɛ\n')
 while(err > tol)
     n++;
-    dfx1 = (((f(x0+(tol/10)))-(f(x0-(tol/10))))/(tol*2/10));
-    if (dfx1 == 0)
-        printf('La derviada de fx es cero, seleccione otro metodo.');
-        err = 0;
-    else
-        x1 = x0 - (f(x0)/ dfx1);
-        x0 = x1;
-        y = f(x1);
-        err = abs(y);
-        fprintf('\t %d  \t%#.6f \t %#.6f \t %#.6f \t %#.6f\n', n, x0, x1 , y, err)
-    endif
+    dfx1 = (((f(x0+(tol/1000)))-(f(x0-(tol/1000))))/(tol*2/1000));
+    d2fx1 = ((f(x0+(tol/1000))) - (2*f(x0)) + (f(x0-(tol/1000))))/(tol*2/1000);
+    x1 = x0 - (f(x0)*dfx1)/((dfx1)^2 -f(x0)*d2fx1);
+    x0 = x1;
+    y = f(x1);
+    err = abs(y);
+    fprintf('\t %d  \t%#.6f \t %#.6f \t %#.6f \t %#.6f\n', n, x0, x1 , y, err)
 endwhile
 printf('\n\nFunción f(x) = %s \n',fun); 
 printf('Raiz: f(%f) = %d \n\n',x0,y);
