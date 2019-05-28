c871eb0824f6351a9f18adf641cb3f1f7ead4c0b
José Andrés Ceballos Vadillo <jacv89@gmail.com>
2019-05-28 09:57:33 -0500

    unidad 6

---
+++ u6/runnkuta.m
@@ -0,0 +1,30 @@
+clear
+close all
+
+f=inline('9.8-0.196*y');
+x0=input('Introduzca el valor inicial para x0: ');
+y0=input('Introduzca el valor inicial para y0: ');
+DeltaX=input('Introduzca el incremento inicial: ');
+Xap=input('Introduzca el valor a aproximar: ');
+
+x=0:DeltaX:Xap
+ysol=50-50*e.^(-0.196*x);
+hold on
+plot(x,ysol,'o-g');
+
+fprintf('RK4\n');
+x0(1)=x0
+y0(1)=y0
+n=1
+while x0 < Xap
+    x0(n+1)=x0(n) + DeltaX
+    K01=f(y0(n),x0(n));
+    K02=f(y0(n)+0.5.*K01.*DeltaX, x0(n)+0.5.*DeltaX);
+    K03=f(y0(n)+0.5.*K02.*DeltaX, x0(n)+0.5.*DeltaX);
+    K04=f(y0(n)+K03.*DeltaX, x0(n)+DeltaX);
+    phi=(1/6)*(K01+2.*K02+2.*K03+K04);
+    y0(n+1)=y0(n)+phi.*DeltaX
+    n=n+1
+end
+
+plot(x0,y0,'-r');
